9d2c1c42c5ae816324a22557ff5987f6ab487f51
lucas choplin <lucas.choplin72@email.com>
2026-03-10 17:58:41 +0100

    petite modification de combat

--- system/combat/combat.c
+++ system/combat/combat.c
@@ -1,9 +1,4 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "structs.h"
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
+#include "combat.h"
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 960
--- system/combat/combat.h
+++ system/combat/combat.h
@@ -2,6 +2,11 @@
 #define COMBAT_H
 
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
 int lancerCombat(SDL_Renderer *renderer);
 
